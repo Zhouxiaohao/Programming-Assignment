fcbe21ef7e7c373bc63c01227d000965aaa73fb1
tracer-ics2015 <tracer@njuics.org>
2017-01-17 16:09:40 +0800

    >  compile NEMU 141220000 zhouxiaohao Linux zhouxiaohao-virtual-machine 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:26 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:09:40 up  4:59,  8 users,  load average: 0.09, 0.07, 0.05 9bc8f6541d132ecc7cfb6c96131af6761c7ee972

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -158,8 +158,8 @@ static int cmd_info(char *args) {
 		printf("Right command 'info' format: 'info r' or 'info w'\n");
 	}
 	else if(!strcmp(args, "r")) {
-		printf("Register\tHexadecimal\tDecimal\n");
-		printf("eax\t0x%8x\t%d\n", cpu.eax, cpu.eax);
+		printf("Register Hexadecimal Decimal\n");
+		printf("eax\t0x%x\t%d\n", cpu.eax, cpu.eax);
 	}
 	else if(!strcmp(args, "w")) {
 
